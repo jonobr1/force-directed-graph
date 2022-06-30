945a384b8373181c574218aecfef5fdd81062936
Jono Brandel <developer@jono.fyi>
2022-06-30 14:56:34 -0700

    Made Fog Optional

--- src/shaders.js
+++ src/shaders.js
@@ -190,6 +190,7 @@ const points = {
 
     varying vec3 vColor;
     varying float vImageKey;
+    varying float vDistance;
 
     attribute float imageKey;
 
@@ -204,6 +205,7 @@ const points = {
       gl_PointSize = nodeRadius * nodeScale;
       gl_PointSize *= mix( 1.0, frustumSize / - mvPosition.z, sizeAttenuation );
 
+      vDistance = 1.0 / - mvPosition.z;
       vColor = color;
       vImageKey = imageKey;
 
@@ -225,6 +227,7 @@ const points = {
 
     varying vec3 vColor;
     varying float vImageKey;
+    varying float vDistance;
 
     float circle( vec2 uv, vec2 pos, float rad ) {
 
@@ -233,7 +236,7 @@ const points = {
       float d = length( pos - uv ) - ( rad - limit );
       float t = clamp( d, 0.0, 1.0 );
 
-      float viewRange = smoothstep( 0.0, frustumSize * 0.001, abs( 1.0 / vFogDepth ) );
+      float viewRange = smoothstep( 0.0, frustumSize * 0.001, abs( vDistance ) );
       float taper = limit2 * viewRange + limit;
       taper = mix( taper, limit2, sizeAttenuation );
 
